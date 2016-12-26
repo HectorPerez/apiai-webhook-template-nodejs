a814052093a89e52d9011ababd84830f2c7843c4
Hector Perez <hecpeare@gmail.com>
2016-12-26 20:50:19 +0100

    generateAnswer

--- app.js
+++ app.js
@@ -21,14 +21,29 @@ let bodyParser = require('body-parser');
 let app = express();
 app.use(bodyParser.json({type: 'application/json'}));
 
+const MIN = 0;
+const MAX = 100;
 const CHECK_GUESS_ACTION = 'check_guess';
 const GENERATE_ANSWER_ACTION = 'generate_answer';
+const GREETING_PROMPTS = ['Let\'s play Number Genie!', 'Welcome to Number Genie!'];
+const INVOCATION_PROMPT = ['I\'m thinking of a number from %s to %s. What\'s your first guess?'];
+const NO_INPUT_PROMPTS = ['I didn\'t hear a number', 'If you\'re still there, what\'s your guess?', 'We can stop here. Let\'s play again soon.'];
 
 // [START YourAction]
 app.post('/', function (req, res) {
   const assistant = new Assistant({request: req, response: res});
   console.log('Request headers: ' + JSON.stringify(req.headers));
   console.log('Request body: ' + JSON.stringify(req.body));
+
+  function generateAnswer (assistant) {
+    console.log('generateAnswer');
+    let answer = getRandomNumber(MIN, MAX);
+    assistant.data.answer = answer;
+    assistant.data.guessCount = 0;
+    assistant.data.fallbackCount = 0;
+    assistant.ask(sprintf(sprintf(getRandomPrompt(GREETING_PROMPTS)) + ' ' +
+      getRandomPrompt(INVOCATION_PROMPT), MIN, MAX), NO_INPUT_PROMPTS);
+  }
 
   function checkGuess (assistant) {
     console.log('checkGuess');
